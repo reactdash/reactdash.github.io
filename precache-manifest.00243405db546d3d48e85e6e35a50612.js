f6da723de0d48764055a9a3cd502e25fad3505d2
Sajith Nandasena <sajith@zmessenger.com>
2019-04-16 12:58:05 +0530

    first commit

---
+++ precache-manifest.00243405db546d3d48e85e6e35a50612.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "41bf3241965cc7c2990174ed9d6e9714",
+    "url": "./static/media/3.41bf3241.jpeg"
+  },
+  {
+    "revision": "313dfe7db2597c4dd716",
+    "url": "./static/css/main.63908d56.chunk.css"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "fe349e8528c79623ea6795ddd110985a",
+    "url": "./static/media/10.fe349e85.jpeg"
+  },
+  {
+    "revision": "09f21616c1c2fd53477c",
+    "url": "./static/js/2.5b10735e.chunk.js"
+  },
+  {
+    "revision": "73476783ca0f7583198978de7073815b",
+    "url": "./static/media/0.73476783.jpg"
+  },
+  {
+    "revision": "60a859917fe65ea1f9f070e0dc080c76",
+    "url": "./static/media/shards-dashboards-logo.60a85991.svg"
+  },
+  {
+    "revision": "ed986061ad9bf89c081724362956b134",
+    "url": "./static/media/1.ed986061.jpeg"
+  },
+  {
+    "revision": "780ccb41c83dad3cbd24a9bcd3262586",
+    "url": "./static/media/2.780ccb41.jpeg"
+  },
+  {
+    "revision": "313dfe7db2597c4dd716",
+    "url": "./static/js/main.1a48f0e6.chunk.js"
+  },
+  {
+    "revision": "026cb7af3133d721efff2a0134f7e16f",
+    "url": "./static/media/4.026cb7af.jpeg"
+  },
+  {
+    "revision": "6e846951d7a04c931a0f9efa94a1316f",
+    "url": "./static/media/5.6e846951.jpeg"
+  },
+  {
+    "revision": "47789d678cfff97c48fda4de3f3621d5",
+    "url": "./static/media/6.47789d67.jpeg"
+  },
+  {
+    "revision": "2835396c930c8bf53ffd3beeedba7c41",
+    "url": "./static/media/7.2835396c.jpeg"
+  },
+  {
+    "revision": "10b775cc930b6986ec69eb9a4adef56a",
+    "url": "./static/media/8.10b775cc.jpeg"
+  },
+  {
+    "revision": "565ad4a387d2090e5d73f0c7b8de2793",
+    "url": "./static/media/9.565ad4a3.jpeg"
+  },
+  {
+    "revision": "09f21616c1c2fd53477c",
+    "url": "./static/css/2.831e2368.chunk.css"
+  },
+  {
+    "revision": "033e9864d17da21029f033f0c55a1908",
+    "url": "./index.html"
+  }
+];
